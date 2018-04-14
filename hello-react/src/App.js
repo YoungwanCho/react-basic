16c58db801c85f32cc7a1e2c3b4ee93687e0273b
developer119 <developer119korea@gmail.com>
2018-04-14 21:55:19 +0900

    컴포넌트에 props를 사용하여 값 전달 해봄

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
+import MyName from './MyName';
 
 class App extends Component {
   render() {
     return (
       <div>
-
+        <MyName name = "리액트"/>
       </div>
     );
   }
