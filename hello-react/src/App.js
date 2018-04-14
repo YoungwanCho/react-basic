1d45c3e09cf39e9d78a3b4665fddff6b13df6fe4
developer119 <developer119korea@gmail.com>
2018-04-14 22:23:47 +0900

    카운터 컴포넌트를 작성

--- hello-react/src/App.js
+++ hello-react/src/App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import MyName from './MyName';
+import Counter from './Counter';
 
 class App extends Component {
   render() {
     return (
       <div>
-        <MyName name = "리액트"/>
+        {/* <MyName name = "리액트"/> */}
+        <Counter/>
       </div>
     );
   }
