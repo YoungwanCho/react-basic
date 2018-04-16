d97df5b6026d9b818b8e8d33e319c8c3b7ebef9f
developer119 <developer119korea@gmail.com>
2018-04-16 10:51:36 +0900

    LifeCycle API를 사용해봤다.

--- hello-react/src/Counter.js
+++ hello-react/src/Counter.js
@@ -1,31 +1,61 @@
 import React, { Component } from 'react';
 
+const Problematic = () => {
+    throw (new Error('버그가 나타났다!'));
+    return (
+        <div>
+
+        </div>
+    );
+};
+
 class Counter extends Component {
     state = {
         number: 0
     }
 
+    constructor(props) {
+        super(props);
+        console.log('constructor');
+    }
+
+    componentWillMount() {
+        console.log('componentWillMount (deprecated)');
+    }
+
+    componentDidMount() {
+        console.log('componentDidMount');
+    }
+
+    componentDidCatch(error, info) {
+        this.setState({
+            error: true
+        });
+    }
+
     handleIncrease = () => {
         this.setState(
             ({ number }) => ({
-                number : number + 1
+                number: number + 1
             })
         );
     }
 
     handleDecrease = () => {
         this.setState(
-            ( {number}) => ({
-                number : number -1
+            ({ number }) => ({
+                number: number - 1
             })
         );
     }
 
     render() {
+        if (this.state.error) return (<h1>에러발생</h1>);
         return (
             <div>
                 <h1>카운트</h1>
                 <div>값: {this.state.number}</div>
+                {this.state.number === 4 && <Problematic />}
                 <button onClick={this.handleIncrease}>+</button>
                 <button onClick={this.handleDecrease}>-</button>
             </div>
